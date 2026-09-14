ebb8c886523fb7d2a779c7f7ab9d97a71fea364c
Rubén Martín <hasdpk@gmail.com>
2026-09-14 02:40:17 +0100

    chore: disable Composer progress output during module installation

--- .docker/prestashop/tmp/pre-install-scripts/composer.sh
+++ .docker/prestashop/tmp/pre-install-scripts/composer.sh
@@ -2,10 +2,11 @@
 set -eu
 
 if [ -f "/var/www/html/modules/$MODULE_NAME/composer.json" ]; then
-    echo "\n* Installing \033[32m$MODULE_NAME\033[0m Composer dependencies"
+    echo "\n* Installing \033[32m$MODULE_NAME\033[0m Composer dependencies\n"
 
     runuser -u www-data -- composer install \
         --working-dir="/var/www/html/modules/$MODULE_NAME" \
         --no-interaction \
+        --no-progress \
         --prefer-dist
 fi
